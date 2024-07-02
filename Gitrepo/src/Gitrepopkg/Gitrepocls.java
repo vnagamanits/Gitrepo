237ea5bd9807c9fbc2b23697282a4937555afe0e
vnagamanits <vnagamanits@gmail.com>
2024-07-02 13:34:27 +0200

    Initial commit

---
+++ Gitrepo/src/Gitrepopkg/Gitrepocls.java
@@ -0,0 +1,12 @@
+package Gitrepopkg;
+
+public class Gitrepocls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Git Repository Location");
+
+	}
+
+}
